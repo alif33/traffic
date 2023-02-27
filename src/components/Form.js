aceaed789ee69a65d05560c9acb04950bf9dcc6f
alif33 <alifhasan332@gmail.com>
2023-02-27 21:06:15 +0600

    ...

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,29 +1,33 @@
-export default function Form() {
+export default function Form({ trafficInfos, setTrafficInfos}) {
+
     return (
         <div className="results-section">
             <div className="results w-50 m-auto">
-                <h3 className="text-center">36 ongoing traffic disruptions</h3>
-                <div className="card p-3 mb-3">
+                {
+                    trafficInfos &&(
+                        <h3 className="text-center mb-5">{trafficInfos.length} ongoing traffic disruptions</h3>
+                    )
+                }
+                
+                {
+                    trafficInfos && trafficInfos.map((info, index)=>(
+                        <div key={index} className="card p-3 mb-3">
+                            <h3 className="card-title">{info.title}</h3>
+                            <h4>{info.startTime} {info.endTime}Updated: 2023-02-10 14:43</h4>
+                            <p>
+                                {info.description}
+                            </p>
+                        </div>
+                    ))
+                }
+            
+                {/* <div className="card p-3 mb-3">
                     <h3 className="card-title">Other traffic information</h3>
                     <h5 className="card-date">Updated: 2023-02-10 14:43</h5>
                     <p>
                         Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC.
                     </p>
-                </div>
-                <div className="card p-3 mb-3">
-                    <h3 className="card-title">Other traffic information</h3>
-                    <h5>Updated: 2023-02-10 14:43</h5>
-                    <p>
-                        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC.
-                    </p>
-                </div>
-                <div className="card p-3 mb-3">
-                    <h3 className="card-title">Other traffic information</h3>
-                    <h5 className="card-date">Updated: 2023-02-10 14:43</h5>
-                    <p>
-                        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC.
-                    </p>
-                </div>
+                </div> */}
             </div>
            
         </div>
--- src/pages/TraffikPage.js
+++ src/pages/TraffikPage.js
@@ -1,12 +1,58 @@
 import Layout from '../base/Layout';
 import Hero from '../components/Hero';
 import Form from '../components/Form';
+import { authPost, __getData } from '../helpers/HttpServices'
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [trafficInfos, setTrafficInfos] = useState();
+  // cosnt [ _count, setCount ] = useState(0);
+
+
+  useEffect(()=>{
+    const getToken = async () => {
+      const response = await fetch('https://api.vasttrafik.se/token?grant_type=client_credentials', {
+        method: 'POST',
+        headers: {
+          'Authorization': 'Basic QUNqVjFvWEhOM3NrblUzY3RTT3ltMHdtMzBzYTpmZm1mbVBmTU5URUQ3YXlRaEplZkp4TDd2Xzhh'
+        }
+      });
+      const data = await response.json();
+      return data.access_token;
+    }
+    
+    // let count = 0;
+  
+  
+    const fetchData = async () => {
+      const token = await getToken();
+      const response = await fetch('https://api.vasttrafik.se/ts/v1/traffic-situations', {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      });
+      const data = await response.json();
+      console.log(data);
+      setTrafficInfos(data);
+      // setCount(_count+1)
+    }
+    
+    fetchData();
+  }, [])
+
+  // console.log(_count);
+  
+
   return (
     <Layout>
-      <Hero/>
-      <Form/>
+      <Hero
+        trafficInfos={trafficInfos}
+        setTrafficInfos={setTrafficInfos}
+      />
+      <Form
+        trafficInfos={trafficInfos}
+        setTrafficInfos={setTrafficInfos}
+      />
     </Layout>
   )
 }
